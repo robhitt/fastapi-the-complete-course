de3037a6ea1f3fead58f70107dfe35556fee78b9
rob.h <rob.h@taboola.com>
2023-08-07 11:38:38 -0400

    complete project1 start project1

---
+++ Project1/books.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Two', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Two', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Six', 'category': 'math'},
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/mybook")
+async def read_all_books():
+    return {'book_title': 'My favorite Book'}
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+# fetch all books from a specific author
+@app.get("/books/get-books-by-author/")
+async def get_books_by_author(author: str):
+    books_by_author = []
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('author').casefold() == author.casefold():
+            books_by_author.append(BOOKS[i])
+
+    return books_by_author
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return BOOKS
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+    return BOOKS
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+    return BOOKS
